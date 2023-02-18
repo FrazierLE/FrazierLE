7673c36dc286e2a7204a8d3b3de4ab0463fac790
Lauren Frazier <lauren.e.frazier@gmail.com>
2023-02-18 15:50:49 -0700

    Add links to card buttons

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -1,21 +1,25 @@
 import React from 'react'
 import { Card, Button } from 'react-bootstrap'
+import './Project.css'
+import { NavLink } from 'react-router-dom'
 
 const Project = ({ id, name, image, repo, deployed, summary, technologies }) => {
  return(
   <Card className='card' style={{width: '20rem', margin: '3rem'}}>
-    <Card.Header className="card-img-top">
-      <img src={image}/>
-    </Card.Header>
+    <Card.Img className="card-img-top" src={image}></Card.Img>
     <Card.Title className="card-title">
       <h2>{name}</h2>
     </Card.Title>
     <Card.Body className="card-body">
       <p className="card-text">{summary}</p>
     </Card.Body>
-    <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
-      <Button style={{height: '2rem', width: 'max-content'}}>Repo</Button>
-      <Button style={{height: '2rem', width: 'max-content'}}>Deployed</Button>
+    <div className='project-btn'>
+      <NavLink to={repo}>
+        <Button style={{height: '2rem', width: 'max-content'}}>Repo</Button>
+      </NavLink>
+      <NavLink to={deployed}>
+        <Button style={{height: '2rem', width: 'max-content'}}>Deployed</Button>
+      </NavLink>
     </div>
   </Card>
  )
