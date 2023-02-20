b824533962c58f102f42679b490ec431bbee572d
Lauren Frazier <lauren.e.frazier@gmail.com>
2023-02-19 17:03:03 -0700

    Update css

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -8,9 +8,9 @@ import profile from '../../assets/profile.jpg'
 const About = () => {
   return (
     <div className='about-container'>
-      <h2>About Me</h2>
+      <h2 style={{marginBottom: '1rem'}}>About Me</h2>
       <div className='about-section'>
-        <div>
+        <div className='about-me'>
           <h3>I'm Lauren, and I'm a front end software developer.</h3>
           <p>Before software development, I was a strength coach. I got my bachelors in exercise science, and trained clients for 7 years. But in January 2022, I was watching the sunrise alone in Costa Rica, and it was a pivotal moment. I knew next steps for me was either to start moving towards starting my own gym or making a career shift. One of my clients was a career-changer-software-developer so I sat him down and now here I am. I enrolled in Turing School of Software & Design in the front end program. When I’m not coding, I’m training clients, snowboarding, enjoying live music, traveling the world, exploring different coffee shops, and seeing friends. </p>
         </div>
--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -6,33 +6,34 @@ const Skills = () => {
   return (
     <div className='skills-container'>
       <Card className='development skills'>
-        <Card.Header style={{borderRadius: '1rem'}}>Development</Card.Header>
+        <Card.Header className='title-header'>Development</Card.Header>
         <div className='flex'>
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/graphql/graphql-plain-wordmark.svg" alt='graphql icon'/>
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt='javascript logo'/> 
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" alt='typescript logo'/>
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" alt='react logo'/>
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/webpack/webpack-original-wordmark.svg" alt='webpack logo'/>
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original-wordmark.svg" alt='express logo'/> 
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg" alt='redux logo'/>
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/graphql/graphql-plain-wordmark.svg" alt='graphql icon' title='GraphQL'/>
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt='javascript logo'title='JavaScript'/> 
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" alt='typescript logo' title='TypeScript'/>
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" alt='react logo' title='React'/>
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/webpack/webpack-original-wordmark.svg" alt='webpack logo' title='Webpack'/>
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original-wordmark.svg" alt='express logo' title='Express'/> 
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg" alt='redux logo' title='Redux'/>
         </div>
         {/* <h3>Not shown -- Apollo Client, REST API</h3> */}
       </Card>
 
       <Card className='workflow skills'>
-        <Card.Header style={{borderRadius: '1rem'}}>Workflow</Card.Header>
+        <Card.Header className='title-header'>Workflow</Card.Header>
         <div className='flex'>
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" alt='github logo'/>
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original-wordmark.svg" alt='git logo'/> 
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg" alt='npm logo'/>
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" alt='github logo' title='Github'/>
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original-wordmark.svg" alt='git logo' title='Git'/> 
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg" alt='npm logo' title='npm'/>
         </div>
         {/* <h3>Not shown -- Github projects, TDD, Agile, Miro, Netlify, Vercel</h3> */}
       </Card>
 
       <Card className='testing skills'>
-        <Card.Header style={{borderRadius: '1rem'}}>Testing</Card.Header>
+        <Card.Header className='title-header'>Testing</Card.Header>
         <div className='flex'>
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mocha/mocha-plain.svg" alt='mocha logo'/>
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mocha/mocha-plain.svg" alt='mocha logo' title='Mocha'/>
+          <img src='https://raw.githubusercontent.com/cypress-io/cypress/develop/assets/cypress-logo-light.png' alt='cypress logo' title='Cypress'/>
         </div>
         {/* <h3>Not shown -- Cypress</h3> */}
       </Card>
@@ -43,13 +44,13 @@ const Skills = () => {
       </div> */}
       
       <Card className='web-design skills'>
-        <Card.Header style={{borderRadius: '1rem'}}>Web Design</Card.Header>
+        <Card.Header className='title-header' >Web Design</Card.Header>
         <div className='flex'>
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original-wordmark.svg" alt='bootstrap logo'/>
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" alt='css logo'/>
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" alt='html logo'/>   
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/trello/trello-plain.svg" alt='trello logo'/>
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg" alt='figma logo'/> 
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original-wordmark.svg" alt='bootstrap logo' title='BootStrap'/>
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" alt='css logo' title='CSS3'/>
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" alt='html logo' title='HTML5'/>   
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/trello/trello-plain.svg" alt='trello logo' title='Trello'/>
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg" alt='figma logo' title='Figma'/> 
         </div>
           {/* <h3>Not shown -- Responsive Design</h3> */}
       </Card>    
